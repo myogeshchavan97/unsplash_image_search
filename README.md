6307f13d184603ff69738ddc9692b9d98502991d
myogeshchavan97 <myogeshchavan97@gmail.com>
2023-06-04 22:28:04 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Steps to run this application
+
+- Add your Unsplash Api key in `.env` file
+- Execute `npm install` or `yarn install` command to install packages
+- Execute `npm run dev` or `yarn run dev` command to start the application
+- Access the application at the URL displayed in the terminal
---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import axios from 'axios';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+const API_URL = 'https://api.unsplash.com/search/photos';
+const IMAGES_PER_PAGE = 20;
+
+function App() {
+  const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const fetchImages = useCallback(async () => {
+    try {
+      if (searchInput.current.value) {
+        setErrorMsg('');
+        const { data } = await axios.get(
+          `${API_URL}?query=${
+            searchInput.current.value
+          }&page=${page}&per_page=${IMAGES_PER_PAGE}&client_id=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
+        setImages(data.results);
+        setTotalPages(data.total_pages);
+      }
+    } catch (error) {
+      setErrorMsg('Error fetching images. Try again later.');
+      console.log(error);
+    }
+  }, [page]);
+
+  useEffect(() => {
+    fetchImages();
+  }, [fetchImages]);
+
+  const resetSearch = () => {
+    setPage(1);
+    fetchImages();
+  };
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    resetSearch();
+  };
+
+  const handleSelection = (selection) => {
+    searchInput.current.value = selection;
+    resetSearch();
+  };
+
+  return (
+    <div className='container'>
+      <h1 className='title'>Image Search</h1>
+      {errorMsg && <p className='error-msg'>{errorMsg}</p>}
+      <div className='search-section'>
+        <Form onSubmit={handleSearch}>
+          <Form.Control
+            type='search'
+            placeholder='Type something to search...'
+            className='search-input'
+            ref={searchInput}
+          />
+        </Form>
+      </div>
+      <div className='filters'>
+        <div onClick={() => handleSelection('nature')}>Nature</div>
+        <div onClick={() => handleSelection('birds')}>Birds</div>
+        <div onClick={() => handleSelection('cats')}>Cats</div>
+        <div onClick={() => handleSelection('shoes')}>Shoes</div>
+      </div>
+      <div className='images'>
+        {images.map((image) => (
+          <img
+            key={image.id}
+            src={image.urls.small}
+            alt={image.alt_description}
+            className='image'
+          />
+        ))}
+      </div>
+      <div className='buttons'>
+        {page > 1 && (
+          <Button onClick={() => setPage(page - 1)}>Previous</Button>
+        )}
+        {page < totalPages && (
+          <Button onClick={() => setPage(page + 1)}>Next</Button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
